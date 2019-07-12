af9bf8c2b7b1a11590970ff7a5988aca830fbbd4
Tien Nhan <tnhan@echidna.ca>
2019-07-12 11:57:51 -0400

    initial commit

---
+++ lib/firebase_ruby.rb
@@ -0,0 +1,6 @@
+require "firebase_ruby/version"
+
+module FirebaseRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/firebase_ruby/version.rb
@@ -0,0 +1,3 @@
+module FirebaseRuby
+  VERSION = "0.1.0"
+end
